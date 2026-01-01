eb1e9ab3dc3d7bde75e63289151eb0ca9abc6266
setasorcer <ryanrlumabao@gmail.com>
2026-01-01 01:23:46 -1000

    Refactor 'fun' settings that didn't make sense, separate virtualisation module

---
+++ modules/core/de/fun.nix
@@ -0,0 +1,37 @@
+{ pkgs, config, lib, inputs, ... }:
+
+let
+  cfghoyo = config.desktop.games.hoyo;
+  cfgsteam = config.desktop.games.steam;
+in
+{
+  imports = [ inputs.aagl.nixosModules.default ];
+
+  options.desktop = {
+    games = {
+      aagl.enable = lib.mkEnableOption "Enable installation of AAGL, used to launch an anime game";
+      steam.enable = lib.mkEnableOption "Enable installation of Steam and other related utilities";
+    };
+  };
+  config = lib.mkMerge [
+    (lib.mkIf cfghoyo.enable {
+      nix.settings = inputs.aagl.nixConfig; # Set up Cachix
+      programs.anime-game-launcher.enable = true;
+    })
+    (lib.mkIf cfgsteam.enable {
+      programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+        gamescopeSession.enable = true;
+        protontricks.enable = true;
+      };
+      programs.gamescope = {
+        enable = true;
+        capSysNice = true;
+      };
+      hardware.steam-hardware.enable = true;
+    })
+  ];
+}
