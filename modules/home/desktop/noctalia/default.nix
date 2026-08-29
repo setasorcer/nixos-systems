90dfa1998708aa615e5dc0dbc70587bbfbcc8b60
setasorcer <ryanrlumabao@gmail.com>
2026-08-29 13:06:40 -1000

    Noctalia v5 config

--- modules/home/desktop/noctalia/default.nix
+++ modules/home/desktop/noctalia/default.nix
@@ -1,6 +1,58 @@
+{ config, ... }:
+
 {
   programs.noctalia = {
     enable = true;
     systemd.enable = true;
+
+    settings = {
+      bar."default" = {
+        position = "left";
+        scale = 1.1;
+        thickness = 44;
+        widget_spacing = 14;
+        background_opacity = config.stylix.opacity.desktop;
+        start = [ "launcher" "wallpaper" "clock" "notifications" "media" "active_window" ];
+        center = [ "workspaces" "privacy" ];
+        end = [ "tray" "clipboard" "brightness" "battery" "control-center" "group:g1" "session" ];
+        capsule_group = [{
+          enabled = true;
+          id = "g1";
+          members = [ "network" "bluetooth" "volume" ];
+        }];
+      };
+
+      accessibility.ui_scale = 1.1;
+      backdrop.enabled = true;
+      brightness.minimum_brightness = 0.01;
+      nightlight.enabled = true;
+      notifications = {
+        history_retention_hours = 72;
+        position = "top_center";
+      };
+      osd = {
+        position = "bottom_center";
+        position_vertical = "bottom_center";
+      };
+      shell = {
+        animation.speed = 1.5;
+        panel.launcher_placement = "attached";
+        launch_apps_as_systemd_services = true;
+        niri_overview_type_to_launch_enabled = true;
+      };
+      theme.templates = {
+        enable_builtin_templates = false;
+        enable_community_templates = false;
+      };
+      widget = {
+        clock.vertical_format = "{:%H\\n%M\\n%a\\n%d\\n%m}";
+        launcher.glyph = "grid-dots";
+        media.hide_when_no_media = true;
+        network.show_label = false;
+        notifications.hide_when_no_unread = true;
+        privacy.hide_inactive = true;
+        workspaces.show_labels = false;
+      };
+    };
   };
 }
